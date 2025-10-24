e442efdbcb47afe12e1e40da3ae806debbd1ad40
catiecodes <catiehart#mac.com>
2025-10-24 12:58:45 -0700

    debugging Netlify Deploy

--- src/App.js
+++ src/App.js
@@ -14,7 +14,8 @@ import {
 // =============================================
 // CONFIGURATION - BACKEND API URL
 // =============================================
-const API_BASE_URL = "http://localhost:3001/api";
+const API_BASE_URL =
+  process.env.REACT_APP_API_BASE_URL || "http://localhost:3001/api";
 
 // =============================================
 // DATE FORMATTER
